0ed18f5729336eeffdb92436eeb1c35e1b35788e
brandiux <brandon.valenzuela247077@potros.itson.edu.mx>
2026-06-03 13:29:27 -0700

    Practica 1

---
+++ Practica1/MiPrimerServidorWeb/MiPrimerServidorWeb/src/main/java/com/example/MiPrimerServidorWeb/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.MiPrimerServidorWeb;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello desde mi controlador SpringBoot";
+    }
+
+    @GetMapping("/saludo")
+    public String saludar(@RequestParam String nombre){
+        return "Hola " + nombre + " este es mi servidor de aplicacion";
+    }
+
+    @PostMapping("/mensaje")
+    public String recibirMensaje(@RequestBody String mensaje){
+        return "Recibi tu mensaje" + mensaje;
+    }
+}
